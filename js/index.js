bb823b6bd92c9bac0ce868e18dfaafefe96fce19
Juan <satelimportadores@gmail.com>
2018-08-16 08:43:39 -0500

    hexadecimal corregido

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //Variables para texto
-  var mensaje_bin = "<strong>Dividir</strong> por<strong> 2 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>. Finalmente tomar los residuos de abajo hacia arriba.";
-  var mensaje_oct = "<strong>Dividir</strong> por<strong> 8 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>. Finalmente tomar los residuos de abajo hacia arriba.";
-  var mensaje_hex = "<strong>Dividir</strong> por<strong> 15 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>. Finalmente tomar los residuos de abajo hacia arriba.";
+  var mensaje_bin = "<strong>Dividir</strong> por<strong> 2 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>.<br> Finalmente tomar los residuos de abajo hacia arriba.";
+  var mensaje_oct = "<strong>Dividir</strong> por<strong> 8 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>.<br> Finalmente tomar los residuos de abajo hacia arriba.";
+  var mensaje_hex = "<strong>Dividir</strong> por<strong> 16 </strong> sucesivamente el valor hasta llegar a <strong>0</strong>. Cuando exista residuo poner un <strong>1</strong>, cuando la division sea exacta poner un <strong>0</strong>.<br> Finalmente tomar los residuos de abajo hacia arriba.";
 //Variables para texto
 
 $(document).on('ready',function(){
@@ -50,7 +50,7 @@ $(document).on('ready',function(){
                               base_numerica = 8;
                             break;
                         case ('Hexadecimal'):
-                              base_numerica = 15;
+                              base_numerica = 16;
                             break;
                         default:
                               base_numerica = 2;
@@ -62,7 +62,7 @@ $(document).on('ready',function(){
                         case (8):
                               $('#mensaje__base').html(mensaje_oct);
                             break;
-                        case (15):
+                        case (16):
                               $('#mensaje__base').html(mensaje_hex);
                             break;
                         default:
@@ -74,12 +74,36 @@ $(document).on('ready',function(){
 
             var numero_temp = n;
             var cadena = ' ';
-            var calculos;
+            var calculos = '';
             while (numero_temp >= 1) {
                 operacion = numero_temp / base_numerica;
                 remainder = numero_temp % base_numerica;
                 calculos = calculos + '<div class="row"><div class="col-md-4"></div><div class="col-md-4"><strong>'+n+'</strong> / <strong>'+numero_temp+'</strong> = <strong>'+operacion+'</strong></div><div class="col-md-4">residuo = <strong>'+remainder+'</strong></div></div>';
                 numero_temp = Math.trunc(operacion);
+                  //convertir numeros superiores a 9 en hexa
+                    if (b == 'Hexadecimal') {
+                           switch (remainder) {
+                                case (10):
+                                      remainder = 'A'
+                                break;
+                                case (11):
+                                      remainder = 'B'
+                                break;
+                                case (12):
+                                      remainder = 'C'
+                                break;
+                                case (13):
+                                      remainder = 'D'
+                                break;
+                                case (14):
+                                      remainder = 'E'
+                                break;
+                                case (15):
+                                      remainder = 'F'
+                                break;
+                           }
+                    }
+                  //convertir numeros superiores a 9 en hexa
                 cadena = cadena + String(remainder);
             }
 
